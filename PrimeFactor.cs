057e165d85640499bdf748775cfca07ba6645d2a
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-09 10:19:53 +0530

    add PrimeFactor.cs

---
+++ PrimeFactor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class PrimeFactor
+    {
+        ///<summary>
+        /// UC1- Prime Factors a number.
+        ///</summary>
+        public static void primefactor()
+        {
+            int a, b;
+            Console.WriteLine("Please enter your integer: ");
+            a = int.Parse(Console.ReadLine());
+            for (b = 1; b <= a; b++)
+            {
+                if (a % b == 0)
+                {
+                    Console.WriteLine(b + " is a factor of " + a);
+                }
+            }
+        }
+    }
+}
