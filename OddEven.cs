df8f37f8452bea69f7d5f08234432957a99e6b4a
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-09 11:10:13 +0530

    add OddEven.cs

---
+++ OddEven.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class OddEven
+    {
+        ///<summary>
+        /// UC1- Finding the number is even or odd.
+        ///</summary>
+        public static void OddorEven()
+        {
+            Console.WriteLine("Enter a Number");
+            int i;
+            i = int.Parse(Console.ReadLine());
+
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("The number is Even");
+            }
+            else
+            {
+                Console.WriteLine("The number is Odd");
+            }
+        }
+    }
+}
