95b5fcb899feb9ddf9d7bb399b3f0be66ddd4110
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-09 11:06:17 +0530

    add LeapYear.cs

---
+++ LeapYear.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class LeapYear
+    {
+        ///<summary>
+        /// UC1- Finding Leap Year.
+        ///</summary>
+        public static void leapyear()
+        {
+            Console.WriteLine("Enter a Year");
+            int year = int.Parse(Console.ReadLine());
+            if (year.ToString().Length < 4)
+            {
+                Console.WriteLine("Enter a valid 4 digit year");
+            }
+            else if (year % 4 == 0)
+            {
+                Console.WriteLine("The Given Year {0} is a Leap Year", year);
+            }
+            else
+            {
+                Console.WriteLine("The Given Year {0} is not a Leap Year", year);
+            }
+        }
+    }
+}
