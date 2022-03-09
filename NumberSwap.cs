16bb8a982040eb6b05a3d320a4ad33ecc18c1618
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-09 11:08:14 +0530

    add NumberSwap.cs

---
+++ NumberSwap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class NumberSwap
+    {
+        ///<summary>
+        /// UC1- Swapping the given numbers.
+        ///</summary>
+        public static void SwapTwoNUmbers()
+        {
+            int a = 9, b = 18;
+            Console.WriteLine("Before swap a= " + a + "b= " + b);
+
+            a = a * b;
+            b = a / b;
+            a = a / b;
+
+            Console.WriteLine("After swap a= " + a + "b= " + b);
+        }
+    }
+}
