9b1d8b3b0c9630b88efaa8714747ae09175f8ef7
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-09 11:12:02 +0530

    add QuotientRemainder.cs

---
+++ QuotientRemainder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class QuotientRemainder
+    {
+        ///<summary>
+        /// UC1- Finding the Quotient and Remainder of a number.
+        ///</summary>
+        public static void FindQuotientAndRemainder()
+        {
+            int x, y;
+            Console.WriteLine("Enter value of x");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter value of y");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = x / y;
+            int remainder = x % y;
+
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+        }
+    }
+}
