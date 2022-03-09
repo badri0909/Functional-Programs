faced68672ed0a56ab0b8c411a774612d07765a1
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-09 11:02:26 +0530

    add HarmonicNumber.cs

---
+++ HarmonicNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class HarmonicNumber
+    {
+        ///<summary>
+        /// UC1- the Finding Harmonic numbers.
+        ///</summary>
+        public static void HarNum()
+        {
+            int i, n;
+            double d = 0.0;
+
+            Console.WriteLine("Enter the value of n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                d += 1 / (float)i;
+            }
+            Console.WriteLine("\nSum of series upto {0} terms : {1} ", n, d);
+        }
+    }
+}
